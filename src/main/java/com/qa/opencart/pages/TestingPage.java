55bf325b7944757ef49b91a3bd48aff1a5b3d98b
Ilakkiya <ilakkiya.vr@gmail.com>
2025-09-18 00:51:18 +0530

    final code

--- src/main/java/com/qa/opencart/pages/TestingPage.java
+++ src/main/java/com/qa/opencart/pages/TestingPage.java
@@ -3,6 +3,9 @@ package com.qa.opencart.pages;
 public class TestingPage {
 	int i=10;
 	String name="Adi";
+	public void stdDetails() {
+		System.out.println("Age::"+i+","+"Name::"+name);
+	}
 
 }
 
