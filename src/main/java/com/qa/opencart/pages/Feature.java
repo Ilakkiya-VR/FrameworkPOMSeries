3f9b9df0ed2d565f86461bdf85c3bb76b3902011
Ilakkiya <ilakkiya.vr@gmail.com>
2025-09-17 17:22:52 +0530

    added feaure f2

--- src/main/java/com/qa/opencart/pages/Feature.java
+++ src/main/java/com/qa/opencart/pages/Feature.java
@@ -1,5 +1,10 @@
 package com.qa.opencart.pages;
 
 public class Feature {
-
+	public void f1() {
+		System.out.println("f1 method");
+	}
+	public void f2() {
+		System.out.println("f2 method");
+	}
 }
